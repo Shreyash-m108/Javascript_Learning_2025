9ca03b81e6ce8c6accdb4de1da530c0148f44461
Shreyash Magdum <158493049+Shreyash-m108@users.noreply.github.com>
2025-05-29 16:50:05 +0000

    29-5-25

--- js-strings.js
+++ js-strings.js
@@ -1,1 +1,12 @@
-console.log("Hello js");
+let nameOne = "Shreyash"
+
+console.log(nameOne.length);
+console.log(nameOne.toUpperCase());
+console.log(nameOne.slice(4,9))
+console.log(nameOne.charAt(5))
+
+let nameTwo = "  Magdum  "
+console.log(nameTwo.trim());
+
+let sentence = "i am want to be a java developer in with outstanding package."
+console.log(sentence.split(' '));
