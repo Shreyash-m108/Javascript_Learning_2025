ccbb6201176fc85941d10d62a9959a495effe90d
Shreyash Magdum <158493049+Shreyash-m108@users.noreply.github.com>
2025-07-09 09:54:29 +0000

    9-7

--- Basics/js-closuers.js
+++ Basics/js-closuers.js
@@ -1,11 +1,5 @@
-function a(){
-    var a =8
-    function b(){
-        console.log(a);
-    }
-    b()
-}
-
-var c = a()
-
-
+// 
+setTimeout(function(){
+    console.log("How are you?");
+},2000)
+console.log("Hey Babe!");
